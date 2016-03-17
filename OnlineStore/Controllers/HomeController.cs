4b30c22e84d055dea5b658fac5945b8f8b1dd2cd
gus07ven <gus07ven@gmail.com>
2016-03-17 10:11:54 -0500

    Dynamic View First Try

--- OnlineStore/Controllers/HomeController.cs
+++ OnlineStore/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using OnlineStore.Models;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Web;
 using System.Web.Mvc;
 
@@ -17,8 +18,66 @@ namespace OnlineStore.Controllers
         {
             var products = new List<Product>
             {
-                new Product { Name = "Cleats" },
-                new Product { Name = "Ball" }         
+                new Product { Id = 1,
+                              Name = "Adidas Soccer Shoes",
+                              Price = 99.99m,
+                              picture ="http://www.bayareakicks.com/wp-content/uploads/2011/03/adidas-soccer-cleats-shoes-5.jpg",
+                              },
+                new Product { Id = 2,
+                              Name = "Venezuela Soccer Shirt",
+                              Price = 69.99m,
+                              picture ="http://image-load-balancer.worldsportshops.com/Images/watermarked_thumbnail.aspx?photoNum=1&t=I&catalog=Soccer&img=71781"
+                              },
+                new Product { Id = 3,
+                              Name = "Soccer Shin Guards",
+                              Price = 19.99m,
+                              picture ="http://assets.academy.com/mgen/00/10269800.jpg"
+                              },
+                new Product { Id = 4,
+                              Name = "Adidas Soccer Ball",
+                              Price = 19.99m,
+                              picture ="http://image-load-balancer.worldsportshops.com/Datafeeds/Graphics/Products/ImageCache/600x600/71662.jpg"
+                              },
+                new Product { Id = 5,
+                              Name = "USA Soccer Shirt",
+                              Price = 69.99m,
+                              picture ="http://worldsoccertalk.com/wp-content/uploads/2014/04/usmnt-world-cup-away-shirt1.jpg"
+                              },
+                new Product { Id = 6,
+                              Name = "Soccer Socks",
+                              Price = 9.99m,
+                              picture ="http://www.yomister.com/image/large/iPZ8kLwDTalalGc9iaVD6=VtkvVwjaIC6=gB/images/liverpool-gk-away-socks-2012-13.jpg"
+                              },
+                new Product { Id = 7 ,
+                              Name = "Shorts",
+                              Price = 12.99m,
+                              picture = "http://www.soccergarage.com/images/T/squadra%2013%20short%20navy-01.jpg"
+                              },
+                new Product { Id = 8 ,
+                              Name = "Videos",
+                              Price = 29.99m,
+                              picture = "http://www.soccermylife.com/wp-content/uploads/2015/02/How-To-Play-Soccer-For-Index-Page.jpg"
+                              },
+                new Product { Id = 9 ,
+                              Name = "Cones",
+                              Price = 29.99m,
+                              picture = "http://ecx.images-amazon.com/images/I/31OfL%2BOywnL._SY300_.jpg"
+                              },
+                new Product { Id = 10 ,
+                              Name = "Brazil shirt",
+                              Price = 69.99m,
+                              picture = "http://www.gogoalshop.hk/images/Brazil/2014-World-Cup-Brazil-Yellow-Soccer-Jersey.jpg"
+                              },
+                new Product { Id = 11 ,
+                              Name = "Netherlands shirt",
+                              Price = 69.99m,
+                              picture = "http://ecx.images-amazon.com/images/I/615Pi3dqOgL._UL1500_.jpg"
+                              },
+                new Product { Id = 12 ,
+                              Name = "Germany shirt",
+                              Price = 69.99m,
+                              picture = "http://cdn3.volusion.com/goz35.avhz4/v/vspfiles/photos/TTYADM35023-2.jpg?1456231556"
+                              },
             };
 
             return View(products);
--- OnlineStore/Models/Product.cs
+++ OnlineStore/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,9 @@ namespace OnlineStore.Models
 {
     public class Product
     {
+        public long Id { get; set; }
         public string Name { get; set; }
-        
+        public decimal Price { get; set; }
+        public string picture { get; set; }
     }
 }
